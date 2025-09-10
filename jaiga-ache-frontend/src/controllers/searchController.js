b83a47d1f510e8c192b50108ef2147a78634483d
Sippy <mansuruntsarah@gmail.com>
2025-09-10 21:40:13 +0600

    Remove old backend and frontend folders

--- jaiga-ache-frontend/src/controllers/searchController.js
+++
@@ -1,6 +0,0 @@
-import { handleSearchLogic } from '../models/searchLogic';
-
-export const handleSearch = async (searchQuery) => {
-    const result = await handleSearchLogic(searchQuery);
-    return result;
-};
